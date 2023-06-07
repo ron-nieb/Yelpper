94361567fe7f7807b919bde4991f3c1f2de78acb
Ronald Barasa <124335079+ron-nieb@users.noreply.github.com>
2023-06-07 13:03:13 +0000

    add home and navbar component

---
+++ Yelpper-Frontend/src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import "../css/home.css"
+
+
+export default function Home() {
+    return (
+        <>
+            <div className="container">
+                <div>
+                   
+                    <div id="coverimg">
+                        <p>Test</p>
+                        <h1>The Perfect Hotel Reviews</h1>
+                        <p>We have reviews from over 100000+ users</p>
+                        <a href="#topreview">Top Review</a>
+                    </div>
+                    
+                </div>
+            </div>
+        </>
+    )
+}
