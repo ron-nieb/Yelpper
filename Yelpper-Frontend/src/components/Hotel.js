563d1db3a4e0c4b6227fdc0069472e3c8a5a7d5a
Ronald Barasa <barasaogoro@gmail.com>
2023-06-08 15:33:25 +0300

    added hotel fuctionalities

--- Yelpper-Frontend/src/components/Hotel.js
+++ Yelpper-Frontend/src/components/Hotel.js
@@ -4,7 +4,7 @@ import Reviews from "./Reviews";
 
 export default function Hotel() {
   const [hotels, setHotels] = useState([]);
-  const [newHotel, setNewHotel] = useState({ name: "", location: "" });
+  const [newHotel, setNewHotel] = useState({ name: "", location: "", amenities: [] });
   const [selectedHotel, setSelectedHotel] = useState(null);
   const [newReview, setNewReview] = useState({ user: "", comment: "" });
 
@@ -30,7 +30,7 @@ export default function Hotel() {
         newHotel
       );
       setHotels([...hotels, response.data]);
-      setNewHotel({ name: "", location: "" });
+      setNewHotel({ name: "", location: "", amenities: [] });
     } catch (error) {
       console.error("Error adding hotel:", error);
     }
@@ -63,95 +63,101 @@ export default function Hotel() {
   };
 
   // Render hotels list
-const renderHotels = () => {
-  return hotels.map((hotel) => (
-    <div key={hotel.id}>
-      <h3>{hotel.name}</h3>
-      <p>{hotel.location}</p>
-      <button onClick={() => setSelectedHotel(hotel)}>Add Review</button>
-      {selectedHotel && selectedHotel.id === hotel.id && renderAddReviewForm(hotel.id)}
-    </div>
-  ));
-};
-
-// Render add review form
-const renderAddReviewForm = (hotelId) => {
-  return (
-    <form onSubmit={() => addReview(hotelId)}>
-      <input
-        type="text"
-        value={newReview.user}
-        onChange={(e) => setNewReview({ ...newReview, user: e.target.value })}
-        placeholder="User"
-      />
-      <input
-        type="text"
-        value={newReview.comment}
-        onChange={(e) => setNewReview({ ...newReview, comment: e.target.value })}
-        placeholder="Comment"
-      />
-      <button type="submit">Add Review</button>
-    </form>
-  );
-};
-
-
- // Render add hotel form
-const renderAddHotelForm = () => {
-  const amenities = ["Wi-Fi", "Parking", "Swimming Pool", "Gym", "Restaurant"];
-
-  const handleAmenityChange = (amenity) => {
-    const isChecked = newHotel.amenities.includes(amenity);
-    const updatedAmenities = isChecked
-      ? newHotel.amenities.filter((a) => a !== amenity)
-      : [...newHotel.amenities, amenity];
-    setNewHotel({ ...newHotel, amenities: updatedAmenities });
+  const renderHotels = () => {
+    return hotels.map((hotel) => (
+      <div key={hotel.id} className="card" style={{ width: "18rem" }}>
+        <img className="card-img-top" src={hotel.image} alt="Hotel" />
+        <div className="card-body">
+          <h5 className="card-title">{hotel.name}</h5>
+          <p className="card-text">{hotel.location}</p>
+          <button
+            onClick={() => setSelectedHotel(hotel)}
+            className="btn btn-primary"
+          >
+            Add Review
+          </button>
+          {selectedHotel && selectedHotel.id === hotel.id && renderAddReviewForm(hotel.id)}
+        </div>
+      </div>
+    ));
   };
 
-  return (
-    <form onSubmit={addHotel}>
-      <input
-        type="text"
-        value={newHotel.name}
-        onChange={(e) => setNewHotel({ ...newHotel, name: e.target.value })}
-        placeholder="Hotel Name"
-      />
-      <input
-        type="text"
-        value={newHotel.location}
-        onChange={(e) => setNewHotel({ ...newHotel, location: e.target.value })}
-        placeholder="Hotel Location"
-      />
-      <input
-        type="text"
-        value={newHotel.description}
-        onChange={(e) => setNewHotel({ ...newHotel, description: e.target.value })}
-        placeholder="Hotel Description"
-      />
-      <input
-        type="text"
-        value={newHotel.image}
-        onChange={(e) => setNewHotel({ ...newHotel, image: e.target.value })}
-        placeholder="Hotel Image URL"
-      />
-      <div>
-        <p>Amenities:</p>
-        {amenities.map((amenity) => (
-          <label key={amenity}>
-            <input
-              type="checkbox"
-              checked={newHotel.amenities.includes(amenity)}
-              onChange={() => handleAmenityChange(amenity)}
-            />
-            {amenity}
-          </label>
-        ))}
-      </div>
-      <button type="submit">Add Hotel</button>
-    </form>
-  );
-};
+  // Render add review form
+  const renderAddReviewForm = (hotelId) => {
+    return (
+      <form onSubmit={() => addReview(hotelId)}>
+        <input
+          type="text"
+          value={newReview.user}
+          onChange={(e) => setNewReview({ ...newReview, user: e.target.value })}
+          placeholder="User"
+        />
+        <input
+          type="text"
+          value={newReview.comment}
+          onChange={(e) => setNewReview({ ...newReview, comment: e.target.value })}
+          placeholder="Comment"
+        />
+        <button type="submit">Add Review</button>
+      </form>
+    );
+  };
 
+  // Render add hotel form
+  const renderAddHotelForm = () => {
+    const amenities = ["Wi-Fi", "Parking", "Swimming Pool", "Gym", "Restaurant"];
+
+    const handleAmenityChange = (amenity) => {
+      const isChecked = newHotel.amenities.includes(amenity);
+      const updatedAmenities = isChecked
+        ? newHotel.amenities.filter((a) => a !== amenity)
+        : [...newHotel.amenities, amenity];
+      setNewHotel({ ...newHotel, amenities: updatedAmenities });
+    };
+
+    return (
+      <form onSubmit={addHotel}>
+        <input
+          type="text"
+          value={newHotel.name}
+          onChange={(e) => setNewHotel({ ...newHotel, name: e.target.value })}
+          placeholder="Hotel Name"
+        />
+        <input
+          type="text"
+          value={newHotel.location}
+          onChange={(e) => setNewHotel({ ...newHotel, location: e.target.value })}
+          placeholder="Hotel Location"
+        />
+        <input
+          type="text"
+          value={newHotel.description}
+          onChange={(e) => setNewHotel({ ...newHotel, description: e.target.value })}
+          placeholder="Hotel Description"
+        />
+        <input
+          type="text"
+          value={newHotel.image}
+          onChange={(e) => setNewHotel({ ...newHotel, image: e.target.value })}
+          placeholder="Hotel Image URL"
+        />
+        <div>
+          <p>Amenities:</p>
+          {amenities.map((amenity) => (
+            <label key={amenity}>
+              <input
+                type="checkbox"
+                checked={newHotel.amenities.includes(amenity)}
+                onChange={() => handleAmenityChange(amenity)}
+              />
+              {amenity}
+            </label>
+          ))}
+        </div>
+        <button type="submit">Add Hotel</button>
+      </form>
+    );
+  };
 
   return (
     <div>
